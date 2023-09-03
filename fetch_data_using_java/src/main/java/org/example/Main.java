146dd234658e442a6e5d213274484373fe1e7bc5
Rohit Tanwar <tanwar@rohitmail.com>
2023-09-03 21:40:46 +0530

    fetch_data_1

---
+++ fetch_data_using_java/src/main/java/org/example/Main.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        URL url=null;
+        HttpURLConnection connection=null;
+        int responseCode=0;
+        String httpUrl=" https://api.nationalize.io/?name=nathaniel";
+
+        try{
+            url=new URL(httpUrl);
+        }
+        catch (Exception e){
+            System.out.println("Problem With the Url.. ");
+        }
+
+        try {
+            connection = (HttpURLConnection) url.openConnection();
+            responseCode = connection.getResponseCode();
+        } catch (Exception e) {
+            System.out.println("connection problem");
+        }
+        if(responseCode == 200) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder apiData = new StringBuilder();
+            String readLine = null;
+
+            while ((readLine = in.readLine()) != null) {
+                apiData.append(readLine);
+            }
+
+            //
+            try {
+                in.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.println(apiData.toString());
+            JSONObject jsonAPIResponse = new JSONObject(apiData.toString());
+
+        }
+        else{
+            System.out.println("Something wrong has happend ....");
+        }
+
+        }
+}
